88757e0dde86a36e1e4cc2a79c7900be1176ef98
Alex Rodriguez <alex.rodriguez@freeverse.io>
2023-07-12 12:05:30 +0200

    Added universeID and a random universeName

--- bulk_generate_identity.js
+++ bulk_generate_identity.js
@@ -12,11 +12,21 @@ const identity = require('freeverse-crypto-js');
 const CSV_PATH = "./universe_owners.csv";
 const NUM_ROWS = 100;
 
+// It is retrieven from here: https://mumbai.polygonscan.com/address/0x30B6492570D73035062f854903b8F3035785Cc7E#readContract#F31
+// It is not allowed to use random numbers for the universe id, they need to be consecutive
+const STARTING_UNIVERSE_ID = 48;
+
 class UniverseOwner {
     constructor() {
         // Generate a randow password 15 characters and numbers length
         const password = generator.generate({
             length: 15,
+            numbers: true
+        });
+
+        // Generate a short universe name
+        const universe_name = generator.generate({
+            length: 5,
             numbers: true
         });
         
@@ -29,20 +39,21 @@ class UniverseOwner {
         this.userPassword = password;
         this.privateKey = newId.privateKey;
         this.publicKey = identity.publicKeyFromPrivateKey(newId.privateKey);
+        this.universeName = "sandbox-" + universe_name;
     }
 
     // Writes the object in a CSV file. It is appended to the existing file
-    saveAsCSV() {
+    saveAsCSV(universe_id) {
       try {
         // Write the headers if the file does not exist
         if (!existsSync(CSV_PATH))
         {
-            const header = `web3address,encryptedId,userPassword,privateKey,publicKey\n`;
+            const header = `universeId,web3address,universeName,encryptedId,userPassword,privateKey,publicKey\n`;
             appendFileSync(CSV_PATH, header);
         }
 
         // Flush the class attributes in a new row
-        const csv = `${this.web3address},${this.encryptedId},${this.userPassword},${this.privateKey},${this.publicKey}\n`;
+        const csv = `${universe_id},${this.web3address},${this.universeName},${this.encryptedId},${this.userPassword},${this.privateKey},${this.publicKey}\n`;
         appendFileSync(CSV_PATH, csv);
       } catch (err) {
         console.error(err);
@@ -55,7 +66,7 @@ const startApp = () => {
     for (var i=0; i<NUM_ROWS; i++)
     {
         const universeOwner = new UniverseOwner();
-        universeOwner.saveAsCSV();
+        universeOwner.saveAsCSV(STARTING_UNIVERSE_ID + i);
     }
 }
 startApp();
