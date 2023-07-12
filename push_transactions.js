d8bd3bba4a50189eb1f8b27c72ce773a07667d97
Alex Rodriguez <alex.rodriguez@freeverse.io>
2023-07-12 19:44:12 +0200

    CSV file iterated

--- push_transactions.js
+++ push_transactions.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
+const fs = require('fs');
+const csv = require('fast-csv');
 const { ethers } = require('ethers');
 
 const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const STORAGE_ADDRESS = process.env.STORAGE_ADDRESS;
 const MULTISIG_ADDRESS = process.env.MULTISIG_ADDRESS;
+const CSV_PATH = "./universe_owners.csv";
 
 const storage = require("./contracts/Storage.json");
 const multisig = require("./contracts/MultiSigWallet.json");
+const { resolve } = require('path');
 
 // provider - Alchemy
 const alchemyProvider = new ethers.AlchemyProvider(network="maticMumbai", API_KEY);
@@ -19,25 +23,50 @@ const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
 const StorageContract = new ethers.Contract(STORAGE_ADDRESS, storage.abi, signer);
 const MultiSigContract = new ethers.Contract(MULTISIG_ADDRESS, multisig.abi, signer);
 
+async function readCSV(file)
+{
+    let data = new Array();
+     
+    await new Promise((resolve, reject) => fs.createReadStream(file)
+      .pipe(csv.parse({ headers: true }))
+      .on('error', error => {console.error(error); reject();})
+      .on('data', row => data.push(row))
+      .on('end', () => {console.log(`CSV file read. ${data.length} rows found`);resolve();})
+      );
+
+    return data;
+}
+
 async function main() {
 
-    // TODO: Read the CSV
-    const createUniverseParams = [48, "0xb5015476E2b6a9c09ac9DCeAfF0048310d7C44af", true, "sandbox-iscJE"];
-    const createUniverseMethod = 'createUniverse';
-    const createUniversePopulated = await StorageContract[createUniverseMethod].populateTransaction(...createUniverseParams);
+    // Process the whole CSV file
+    var rows = await readCSV(CSV_PATH);
+    for (var i=0; i<rows.length; i++)
+    {
+        const createUniverseParams = [
+            rows[i]["universeId"], 
+            rows[i]["web3address"], 
+            true, 
+            rows[i]["universeName"]
+        ];
+        const createUniverseMethod = 'createUniverse';
+        const createUniversePopulated = await StorageContract[createUniverseMethod].populateTransaction(...createUniverseParams);
+    
+        // This is the signature that the transaction expects
+        const createUniverseEncodedABI = createUniversePopulated.data;
+    
+        console.log(`Encoded ABI: ${createUniverseEncodedABI} for universeId ${rows[i]["universeId"]}`);
 
-    // This is the signature that the transaction expects
-    const createUniverseEncodedABI = createUniversePopulated.data;
-
-    console.log("Encoded ABI: " + createUniversePopulated.data);
-
-    /*
-    console.log("Submitting the tx...");
-    const tx = await MultisigContract.submitTransaction(STORAGE_ADDRESS, 0, createUniverseEncodedABI);
-    await tx.wait();
-
-    console.log(`Universe ${47} request created.`);
-    */
+    
+        /*
+        console.log("Submitting the tx...");
+        const tx = await MultisigContract.submitTransaction(STORAGE_ADDRESS, 0, createUniverseEncodedABI);
+        await tx.wait();
+    
+        console.log(`Universe ${47} request created.`);
+        */
+    
+    }
 }
 
 main();
