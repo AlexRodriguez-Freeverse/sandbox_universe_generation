76b09db23a0a1e0db6e97d78ae15e178efec690b
Alex Rodriguez <alex.rodriguez@freeverse.io>
2023-07-12 11:14:26 +0200

    Initial commit

---
+++ bulk_generate_identity.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-console */
+
+/*
+Generates a brand new identity and encrypts it with a randomly generated password.
+Use at your own risk. The generated privKey will be output in console.
+*/
+
+const { appendFileSync, existsSync } = require('fs');
+const generator = require('generate-password');
+const identity = require('freeverse-crypto-js');
+
+const CSV_PATH = "./universe_owners.csv";
+const NUM_ROWS = 100;
+
+class UniverseOwner {
+    constructor() {
+        // Generate a randow password 15 characters and numbers length
+        const password = generator.generate({
+            length: 15,
+            numbers: true
+        });
+        
+        // Populate the fields with the information of the newly created account
+        const newId = identity.createNewAccount();
+        const encrypted = identity.encryptIdentity(newId.privateKey, password);
+        
+        this.web3address = newId.address;
+        this.encryptedId = encrypted;
+        this.userPassword = password;
+        this.privateKey = newId.privateKey;
+        this.publicKey = identity.publicKeyFromPrivateKey(newId.privateKey);
+    }
+
+    saveAsCSV() {
+      try {
+        // Write the headers if the file does not exist
+        if (!existsSync(CSV_PATH))
+        {
+            const header = `web3address,encryptedId,userPassword,privateKey,publicKey\n`;
+            appendFileSync(CSV_PATH, header);
+        }
+
+        // Flush the class attributes in a new row
+        const csv = `${this.web3address},${this.encryptedId},${this.userPassword},${this.privateKey},${this.publicKey}\n`;
+        appendFileSync(CSV_PATH, csv);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
+// It creates 100 universe owners
+const startApp = () => {
+
+    for (var i=0; i<NUM_ROWS; i++)
+    {
+        const universeOwner1 = new UniverseOwner();
+        universeOwner1.saveAsCSV();
+    }
+}
+startApp();
