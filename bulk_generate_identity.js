012be6cc45af0a2884bc7b3fb698bce58707e90d
Alex Rodriguez <alex.rodriguez@freeverse.io>
2023-07-13 13:24:52 +0200

    Transactions tested in dev

--- bulk_generate_identity.js
+++ bulk_generate_identity.js
@@ -5,16 +5,17 @@ Generates a series of brand new identity and encrypts it with a randomly generat
 It flushes everything on a CSV file. Use at your own risk. The generated information is very sensitive.
 */
 
+require('dotenv').config();
 const { appendFileSync, existsSync } = require('fs');
 const generator = require('generate-password');
 const identity = require('freeverse-crypto-js');
 
-const CSV_PATH = "./universe_owners.csv";
-const NUM_ROWS = 100;
+const CSV_PATH = process.env.CSV_PATH;
+const NUM_ROWS = process.env.NUM_ROWS;
 
 // It is retrieven from here: https://mumbai.polygonscan.com/address/0x30B6492570D73035062f854903b8F3035785Cc7E#readContract#F31
 // It is not allowed to use random numbers for the universe id, they need to be consecutive
-const STARTING_UNIVERSE_ID = 48;
+const STARTING_UNIVERSE_ID = process.env.STARTING_UNIVERSE_ID;
 
 class UniverseOwner {
     constructor() {
@@ -66,7 +67,7 @@ const startApp = () => {
     for (var i=0; i<NUM_ROWS; i++)
     {
         const universeOwner = new UniverseOwner();
-        universeOwner.saveAsCSV(STARTING_UNIVERSE_ID + i);
+        universeOwner.saveAsCSV(parseInt(STARTING_UNIVERSE_ID) + i);
     }
 }
 startApp();
